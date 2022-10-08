60b559bd6dc48712e9ab50bb8bd85f95372f3be7
MyMac <MyMac@MyMac.lan>
2022-10-08 20:14:09 +0600

    class view

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -16,14 +16,26 @@ def books(request):
     return Response('list of the books', status=status.HTTP_200_OK) 
 
 class BookList(APIView):
-    def get(self, request, format=None):
+    def get(self, request):
+        author = request.GET.get('author')
+        if(author):
+            return Response({"message":"list of the books by " + author}, status.HTTP_200_OK)    
+
         return Response({"message":"list of the books"}, status.HTTP_200_OK)
         # return Response(request.GET.get('title'), status.HTTP_200_OK)
     
-    def post(self, request, format=json):
+    def post(self, request):
+        return Response({"title":request.data.get('title')}, status.HTTP_201_CREATED)
         return Response({"message":"new book created"}, status.HTTP_201_CREATED)
         # return Response(request.data.get('title'), status.HTTP_200_OK)
         
     # def put(self, request, format=json):
     #     return Response({"message":"book updated"}, status.HTTP_200_OK)
     #     return Response(request.data.get('title'), status.HTTP_200_OK)
+
+class Book(APIView):
+    def get(self, request, pk):
+        return Response({"message":"single book with id " + str(pk)}, status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        return Response({"title":request.data.get('title')}, status.HTTP_200_OK)
