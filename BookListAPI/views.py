36a05b7f807c8bc442cb0844f8a0ea14eddfea60
MyMac <MyMac@MyMac.lan>
2022-10-08 16:38:18 +0600

    Decorator

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -4,6 +4,6 @@ from rest_framework import status
 from rest_framework.decorators import api_view 
  # Create your views here. 
 
-@api_view(['POST']) 
+@api_view(['GET','POST']) 
 def books(request): 
     return Response('list of the books', status=status.HTTP_200_OK) 
