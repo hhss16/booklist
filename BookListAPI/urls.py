c13a2c6c7f39845872ada446cb8b10d3d705c035
Hasin Hayder <hasin@leevio.com>
2022-10-29 20:24:42 +0600

    Router

--- BookListAPI/urls.py
+++ BookListAPI/urls.py
@@ -1,15 +1,25 @@
-from django.urls import path 
-from . import views 
+from django.urls import path
+from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 
 router = DefaultRouter(trailing_slash=False)
-router.register('books', views.BookView,basename="books")
-urlpatterns = [ 
-    # path('books/',views.books) ,
-    # path('books/',views.BookList.as_view()),
-    # path('books/<int:pk>',views.Book.as_view())
-    # path('books', views.BookView.as_view({'get':'list','post':'create'}))
-    path('orders', views.Orders.listOrders)
-] 
+router.register('books', views.BookView, basename='books')
+# urlpatterns = [
+#     path('books', views.BookView.as_view(
+#         {
+#             'get': 'list',
+#             'post': 'create',
+#         })
+#     ),
+#     path('books/<int:pk>',views.BookView.as_view(
+#         {
+#             'get': 'retrieve',
+#             'put': 'update',
+#             'patch': 'partial_update',
+#             'delete': 'destroy',
+#         })
+#     )
+# ]
 
-urlpatterns += router.urls
+urlpatterns = router.urls
--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -36,18 +36,26 @@ class BookList(APIView):
     #     return Response({"message":"book updated"}, status.HTTP_200_OK)
     #     return Response(request.data.get('title'), status.HTTP_200_OK)
 
-# class Book(APIView):
+# class BookView(APIView):
 #     def get(self, request, pk):
 #         return Response({"message":"single book with id " + str(pk)}, status.HTTP_200_OK)
 
 #     def put(self, request, pk):
 #         return Response({"title":request.data.get('title')}, status.HTTP_200_OK)
 
-class BookView(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+class BookView(viewsets.ViewSet):
     def list(self, request):
-        return Response({"message":"All books with"}, status.HTTP_200_OK)
+        return Response({"message":"All books"}, status.HTTP_200_OK)
+    def create(self, request):
+        return Response({"message":"Creating a book"}, status.HTTP_201_CREATED)
+    def update(self, request, pk=None):
+        return Response({"message":"Updating a book"}, status.HTTP_200_OK)
+    def retrieve(self, request, pk=None):
+        return Response({"message":"Displaying a book"}, status.HTTP_200_OK)
+    def partial_update(self, request, pk=None):
+        return Response({"message":"Partially updating a book"}, status.HTTP_200_OK)
+    def destroy(self, request, pk=None):
+        return Response({"message":"Deleting a book"}, status.HTTP_200_OK)
     
 class Orders():
     @staticmethod
